380d0fcc5973f1afec3e633a70902320b2961dab
pablomgProgOO <143107479+pablomgProgOO@users.noreply.github.com>
2025-04-28 23:28:04 -0400

    workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/tp02/",
   test: {
     environment: 'jsdom', // THIS is the critical line
   },
